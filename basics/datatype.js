58d6a57dcf810b90d559a12ae605ec9b4e87f320
MONIKAvv <monikalonewolf35@gmail.com>
2024-12-16 10:10:28 +0530

    commit

---
+++ basics/datatype.js
@@ -0,0 +1,21 @@
+"use strict"; // treate all js code as newer version
+
+// alert("hello"); // we are using nodejs not browser
+
+/* Note:- documentation --> tct9 , mdn
+          bigint---> in trading app 
+          string ---> double quotes
+          boolean ---> true or false
+          null ---> stand alone value hai, assign (value aai hi nhi h)
+          undefined ---> when value is not defined, value nhi diya h
+          symbol ---> used for uniqueness 
+
+          object ---> 
+*/
+let Name = "Monika";
+let age = 45;
+let login = true;
+console.table([typeof(Object), typeof(age)]);
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const account_id = 1434353;
+let account_email = "email@gmail.com";
+var account_password = "89977";
+account_city = "jaipur";
+let account_state;
+// account_id = 2; --> not allowed
+
+// Notes: - please please don't use var keyword---> issue in block and functional scope
+
+account_email = "jhghhg@gmail.com";
+account_password = "4543";
+account_city = "nahglore";
+
+console.log(account_id);
+console.table([account_email, account_password, account_city, account_state]);
